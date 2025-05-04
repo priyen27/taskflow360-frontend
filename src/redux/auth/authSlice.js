b0bf5f28b104cf1acfb2cf8a3415f48bd9256199
krishi mehta <mehtakrishi87@gmail.com>
2025-05-04 21:15:14 +0530

    Added api url

--- src/redux/auth/authSlice.js
+++ src/redux/auth/authSlice.js
@@ -1,7 +1,7 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api/auth';
+const API_URL = 'https://taskflow360-backend.onrender.com/api/auth';
 
 const userFromStorage = JSON.parse(localStorage.getItem("user"));
 
--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:5000/api';
+const API_URL = 'https://taskflow360-backend.onrender.com/api';
 
 // Create axios instance with default config
 const api = axios.create({
